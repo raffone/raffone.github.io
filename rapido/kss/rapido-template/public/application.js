d8433244ef3d976d506a4ed777faa8758012aec0
Raffaele Rasini <raffaele.rasini@gmail.com>
2013-07-22 01:16:14 +0200

    More styleguide

---
+++ rapido/kss/rapido-template/public/application.js
@@ -0,0 +1,194 @@
+$(document).ready(function () {
+
+	// $('.dropdown-toggle').dropdown()
+
+	// $('[data-loading-text]').click(function () {
+ //        var btn = $(this)
+ //        btn.button('loading')
+ //        setTimeout(function () {
+ //            btn.button('reset')
+ //        }, 3000)
+ //    });
+
+	// Responsive nav
+	// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+		// Slide
+		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+		// var navigation = responsiveNav("#nav");
+
+
+		// Off canvas
+		// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+		// var navigation = responsiveNav("#nav", {
+		// 	animate: false,
+		// 	offcanvas: true,
+		// 	open: function(){
+		// 		$("body").addClass("nav-open");
+		// 	},
+		// 	close: function(){
+		// 	    $("body").removeClass("nav-open");
+		// 	}
+		// });
+
+
+
+	// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+	// function moveNavbar() {
+	// 	if ( $(".nav--responsive").css("overflow") == 'hidden' ) {
+	// 		$("#nav-destination").append($("#nav-origin .nav"));
+	// 	} else {
+	// 		$("#nav-origin").append($("#nav-destination .nav"));
+	// 	}
+	// }
+
+	// moveNavbar();
+
+	// $(window).resize(function() { moveNavbar(); });
+
+	//  Slider
+	// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+	// $('.bxslider').bxSlider({
+	// 	nextText: '<i></i><span>Next</span>',
+	//   prevText: '<i></i><span>Prev</span>',
+	// 	mode: 'fade',
+	// 	captions: true,
+	// 	pager: false
+	// });
+
+
+
+	// $('.bxslider3').bxSlider({
+	// 	nextText: '<i></i><span>Next</span>',
+	// 	prevText: '<i></i><span>Prev</span>',
+	// 	minSlides: 3,
+	// 	maxSlides: 3,
+	// 	slideWidth: 250,
+	// 	slideMargin: 20
+	// });
+
+
+
+	// $(".various").fancybox({
+	// 	// maxWidth	: 800,
+	// 	// maxHeight	: 600,
+	// 	// fitToView	: false,
+	// 	// width		: '70%',
+	// 	// height		: '70%',
+	// 	// autoSize	: false,
+	// 	// closeClick	: false,
+	// 	// openEffect	: 'none',
+	// 	// closeEffect	: 'none'
+	// });
+
+	// // Tabs
+	// // ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+	// $('#tab-container').easytabs();
+
+
+	// //  Fancybox
+	// // ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+	// $("#single_1").fancybox({
+	// 	helpers: {
+	// 		title : {
+	// 			type : 'float'
+	// 		}
+	// 	}
+	// });
+
+	// $("#single_2").fancybox({
+	// 	openEffect  : 'elastic',
+	// 	closeEffect : 'elastic',
+
+	// 	helpers : {
+	// 		title : {
+	// 			type : 'inside'
+	// 		}
+	// 	}
+	// });
+
+	// $("#single_3").fancybox({
+	// 	openEffect : 'none',
+	// 	closeEffect : 'none',
+	// 	helpers : {
+	// 		title : {
+	// 			type : 'outside'
+	// 		}
+	// 	}
+	// });
+
+	// $("#single_4").fancybox({
+	// 	helpers : {
+	// 		title : {
+	// 			type : 'over'
+	// 		}
+	// 	}
+	// });
+
+	// $(".fancybox-thumb").fancybox({
+	// 	prevEffect  : 'none',
+	// 	nextEffect  : 'none',
+	// 	helpers : {
+	// 		title : {
+	// 			type: 'outside'
+	// 		},
+	// 		thumbs  : {
+	// 			width : 50,
+	// 			height  : 50
+	// 		}
+	// 	}
+	// });
+
+	// $(".fancybox").fancybox({
+	// 	openEffect  : 'none',
+	// 	closeEffect : 'none'
+	// });
+
+
+	// $("[data-toggle=modal]").fancybox({
+	// 	maxWidth  : 800,
+	// 	maxHeight : 600,
+	// 	fitToView : false,
+	// 	width   : '70%',
+	// 	height	: '70%',
+	// 	autoSize  : false,
+	// 	closeClick  : false,
+	// 	openEffect  : 'none',
+	// 	closeEffect : 'none'
+	// });
+
+
+
+	// $(".fancybox-button").fancybox({
+	// 	prevEffect	: 'none',
+	// 	nextEffect	: 'none',
+	// 	closeBtn	: false,
+	// 	helpers   : {
+	// 		title : { type : 'inside' },
+	// 		buttons : {}
+	// 	}
+	// });
+
+	// $(".fancybox-thumb").fancybox({
+	// 	prevEffect  : 'none',
+	// 	nextEffect  : 'none',
+	// 	helpers : {
+	// 		title : {
+	// 			type: 'outside'
+	// 		},
+	// 		thumbs  : {
+	// 			width : 50,
+	// 			height  : 50
+	// 		}
+	// 	}
+	// });
+
+
+});
