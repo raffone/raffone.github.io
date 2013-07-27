310b702973abc05f2745884274d30a25817b3e28
Raffaele Rasini <raffaele.rasini@gmail.com>
2013-07-27 13:00:07 +0200

    Fix safari blank page error

--- rapido/kss/rapido-template/public/config.rb
+++ rapido/kss/rapido-template/public/config.rb
@@ -8,7 +8,8 @@ images_dir = "."
 javascripts_dir = "."
 fonts_dir = "."
 
-output_style = :compressed
+output_style = :compact
+# output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
