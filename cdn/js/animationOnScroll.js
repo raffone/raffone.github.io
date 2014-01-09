d1121303a88e5154a6911fe8aabdfc08b19a664d
Raffaele Rasini <raffaele.rasini@gmail.com>
2014-01-09 23:44:47 +0100

    Fix indentation

--- cdn/js/animationOnScroll.js
+++ cdn/js/animationOnScroll.js
@@ -1,10 +1,10 @@
-	$(window).scroll(function() {
-		$('[data-animation-scroll]').each(function(){
-      var elemPos = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-			if (elemPos < topOfWindow+400) {
-				var animName = $(this).data("animation-scroll");
-				$(this).addClass(animName);
-			}
-		});
-	});
+$(window).scroll(function() {
+  $('[data-animation-scroll]').each(function(){
+    var elemPos = $(this).offset().top;
+    var topOfWindow = $(window).scrollTop();
+    if (elemPos < topOfWindow+400) {
+      var animName = $(this).data("animation-scroll");
+      $(this).addClass(animName);
+    }
+  });
+});
