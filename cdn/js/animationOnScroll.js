b56b8891ebae62f14e24dba77f495de61a83334c
Raffaele Rasini <raffaele.rasini@gmail.com>
2014-01-09 23:43:42 +0100

    JS for showing entrance animations only when element is visible

---
+++ cdn/js/animationOnScroll.js
@@ -0,0 +1,10 @@
+	$(window).scroll(function() {
+		$('[data-animation-scroll]').each(function(){
+      var elemPos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+			if (elemPos < topOfWindow+400) {
+				var animName = $(this).data("animation-scroll");
+				$(this).addClass(animName);
+			}
+		});
+	});
